c00ac64d9cf05a106cb4e3b8d9b0e708255a08c2
Alejosantos007 <alejosantos.2024@gmail.com>
2025-07-25 01:44:00 -0300

    Inicio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,44 @@
-const options = {method: 'GET', headers: {accept: 'application/json'}};
+async function buscarCancion() {
+    const query = document.getElementById("busqueda").value;
+const url = `https://itunes.apple.com/search?term=${encodeURIComponent(query)}&limit=5`;
 
-fetch('https://api.themoviedb.org/3/authentication', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
 
-  
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const contenedor = document.getElementById("resultados");
+    contenedor.innerHTML = "";
+
+			/*SI NO HAY RESULTADOS, SE MUESTRA ESTE MENSAJE */
+    if (data.results.length === 0) {
+        contenedor.innerHTML = "<p>No se encontraron resultados.</p>";
+        return;
+    }
+
+			/*AÑADE AL INDEX CANCIONES QUE FUERON ENCONTRADAS CON RELACION A LO BUSCADO*/
+   data.results.forEach(cancion => {
+        contenedor.innerHTML += `
+            <div class="resultados">
+                <img src="${cancion.artworkUrl100}" alt="Cover">
+                <div>
+                    <strong>${cancion.trackName}</strong><br>
+                    <em>${cancion.artistName}</em><br>
+                    <audio controls src="${cancion.previewUrl}"></audio>
+                </div>
+            </div>
+        `;
+    });
+}
+
+			/*REALIZA LA ACCION DE BUSCAR SIN NECESIDAD DE CLICKEAR EL BOTON DE BUSCAR, SOLAMENTE APRETANDO LA TECLA ENTER*/
+	document.getElementById("busqueda").addEventListener("keydown", function(e) {
+  	if (e.key === "Enter") {
+    	buscarCancion();
+	}
+});
+
+/*LIMPIA EL CONTENIDO LA PAGINA SE REINICIA*/
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("busqueda").value = "";
+    buscarCancion();
+});
